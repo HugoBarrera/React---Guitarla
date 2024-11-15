8284c39d10e96e9cc575b706f60efb849a3656c2
Hugo Barrera <exailer89@gmail.com>
2024-11-15 15:19:50 -0600

    Mostrando información de cada Gruitarra

--- src/App.jsx
+++ src/App.jsx
@@ -19,11 +19,10 @@ function App() {
 		<main className="container-xl mt-5">
 			<h2 className="text-center">Nuestra Colección</h2>
 			<div className="row mt-5">
-				{data.map((item, index) => (
+				{data.map((guitar) => (
 					<Guitarra
-						key={index}
-						price={100}
-						author={true}
+						key={guitar.id}
+						guitar={guitar}
 					/>
 				))}
 			</div>
